e58e2f1824927c8d5a00c79ef939817c89792964
Siddhant-tec <siddhanttiwari200@gmail.com>
2021-07-28 14:01:34 +0530

    added background color

--- main.py
+++ main.py
@@ -1,6 +1,20 @@
 import pygame
 import sys
 
-def run_game:
+
+def run_game():
     pygame.init()
-    main_screen = pygame.display.set_mode()
+    screen = pygame.display.set_mode((1280, 720))
+    pygame.display.set_caption("Alien Invasion")
+    bg_color = (230, 230, 230)
+
+    while True:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                sys.exit()
+
+        screen.fill(bg_color)
+        pygame.display.flip()  # update the screen
+
+
+run_game()
