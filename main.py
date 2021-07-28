8dd61dda88f3a9b60a43cf42af2f069c10d37575
Siddhant-tec <siddhanttiwari200@gmail.com>
2021-07-28 14:38:01 +0530

    Created a seperate settings.py for storing game settings

--- main.py
+++ main.py
@@ -1,19 +1,19 @@
 import pygame
 import sys
-
+from settings import Settings
 
 def run_game():
     pygame.init()
-    screen = pygame.display.set_mode((1280, 720))
+    set = Settings()
+    screen = pygame.display.set_mode((set.screen_width, set.screen_height))
     pygame.display.set_caption("Alien Invasion")
-    bg_color = (230, 230, 230)
 
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 sys.exit()
 
-        screen.fill(bg_color)
+        screen.fill(set.bg_color)
         pygame.display.flip()  # update the screen
 
 
---
+++ settings.py
@@ -0,0 +1,6 @@
+class Settings():
+    def __init__(self):
+        self.screen_width = 1280
+        self.screen_height = 720
+        self.bg_color = (230, 230, 230)
+
