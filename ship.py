5adf5caa7df819beb31f4c09c5c41ad2eac03353
Siddhant-tec <siddhanttiwari200@gmail.com>
2021-07-31 00:37:36 +0530

    Added Restricted movement in the display Functionality

--- ship.py
+++ ship.py
@@ -19,10 +19,10 @@ class Ship():
         self.moving_left = False
 
     def update(self):
-        if self.moving_right:
+        if self.moving_right and self.rect.right < self.screen_rect.right:
             self.rect.centerx += 1
             self.center += self.set.ship_speed_factor
-        if self.moving_left:
+        if self.moving_left and self.rect.left > 0:
             self.rect.centerx -= 1
             self.center += self.set.ship_speed_factor
 
